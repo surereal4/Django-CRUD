a2332717e70c8e0783c04587990beea715292db9
Ridwan Sulaiman <105246901+surereal4@users.noreply.github.com>
2022-06-24 09:49:21 +0100

    Correction

--- I4G010225HT4/blog/views.py
+++ I4G010225HT4/blog/views.py
@@ -11,7 +11,7 @@ class PostListView(generic.ListView):
 class PostCreateView(generic.CreateView):
     model = Post
     fields = "__all__"
-    success_url: reverse_lazy("blog:all")
+    success_url = reverse_lazy("blog:all")
 
 class PostDetailView(generic.DetailView):
     model = Post
@@ -19,10 +19,10 @@ class PostDetailView(generic.DetailView):
 class PostUpdateView(generic.UpdateView):
     model = Post
     fields = "__all__"
-    success_url: reverse_lazy("blog:all")
+    success_url = reverse_lazy("blog:all")
 
 class PostDeleteView(generic.DeleteView):
     model = Post
     fields = "__all__"
-    success_url: reverse_lazy("blog:all")
+    success_url = reverse_lazy("blog:all")
 
